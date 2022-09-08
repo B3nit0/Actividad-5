4d4f1889a0a078943aa35e7a5bdd6e00ef020a85
B3nit0 <joellozano98@hotmail.com>
2022-09-08 16:08:37 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    public static void main(String[] args) {
+        int Numero1 = 70;
+        int Numero2 = 35;
+
+        int suma = Numero1 + Numero2;
+        int resta = Numero1 - Numero2;
+        int multiplicacion = Numero1 * Numero2;
+        int division = Numero1 / Numero2;
+
+        System.out.println("El resultado de la suma es: " + suma + "\nLa resta es: " + resta + "\nLa multiplacion es: " + multiplicacion + "\nLa division es: " + division);
+    }
+}
+
